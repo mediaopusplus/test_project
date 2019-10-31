bc04b81642702d87e404759b830e158460a1e18c
mop-takemori <49058952+mop-takemori@users.noreply.github.com>
2019-10-31 15:44:48 +0900

    tmp

--- asfc/test.py
+++ asfc/test.py
@@ -1,4 +1,4 @@
-{\rtf1\ansi\ansicpg932\cocoartf1561\cocoasubrtf600
+{\rtf1\ansi\ansicpg932\cocoartf1671\cocoasubrtf200
 {\fonttbl\f0\fswiss\fcharset0 Helvetica;}
 {\colortbl;\red255\green255\blue255;}
 {\*\expandedcolortbl;;}
@@ -6,6 +6,7 @@
 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
 \f0\fs24 \cf0 Import pandas as pd\
+Import numpy as np\
 \
 def main():\
 	print(\'91Hello!\'92)\
